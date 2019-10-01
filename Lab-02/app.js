c1968eb37e2dad96df05e53626bcbbc9db32bc9e
root <root@JCM832-L.localdomain>
2019-10-01 14:13:38 -0700

    second question

--- Lab-02/app.js
+++ Lab-02/app.js
@@ -1,4 +1,19 @@
 'use strict'
 var firstName = prompt('What is your name?');
-console.log('firstName', firstName);
+console.log('firstName: ', firstName);
 
+alert('Hello ' + firstName + "!");
+
+var stateQuestion= prompt('Am I from Florida?');
+stateQuestion = stateQuestion.toLowerCase();
+
+if (stateQuestion === 'y' || stateQuestion === 'yes') {
+  alert('Ahh! Wrong! I am from the great state of Texas.');
+} 
+if (stateQuestion === 'n' || stateQuestion === 'no') {
+  alert('You are correct. Thank you.');
+  console.log("You are correct!");
+}
+ else {
+  alert('I don\'t know what that means, but okay.');
+}
