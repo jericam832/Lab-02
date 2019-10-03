1d6c2c2cdb74c1d5c6ed9a0e2408995f9812f4eb
jericam832 <jericam32@gmail.com>
2019-10-02 22:33:02 -0700

    Added question 7.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 'use strict'
 var firstName = prompt('What is your name?');
+var count = 0;
 // console.log('firstName: ', firstName);
 
-alert('Hello ' + firstName + "!");
+alert('Hello ' + firstName + '!');
 //Question 1
 var stateQuestion= prompt('1. Am I from Florida?');
 stateQuestion = stateQuestion.toLowerCase();
@@ -12,7 +13,8 @@ if (stateQuestion === 'y' || stateQuestion === 'yes') {
 } 
 else if (stateQuestion === 'n' || stateQuestion === 'no') {
   alert('You are correct. Thank you.');
-  // console.log("You are correct!");
+  count++;
+  // console.log('You are correct!');
 }
  else {
   alert('I don\'t know what that means, but okay.');
@@ -23,6 +25,7 @@ favoriteColor = favoriteColor.toUpperCase();
 
 if (favoriteColor === 'Y' || favoriteColor === 'YES') {
   alert('You are correct!');
+  count++;
   // console.log('Correct!');
 } else if (favoriteColor === 'N' || favoriteColor === 'NO') {
   alert('Um...wrong! I love the color red.');
@@ -35,6 +38,7 @@ favoriteFood = favoriteFood.toLowerCase();
 
 if (favoriteFood === 'n' || favoriteFood === 'no') {
   alert('4.Yep. You know me so well. I love burgers!');
+  count++;
   // console.log('Correct!');
 } else if (favoriteFood === 'y' || favoriteFood === 'yes'){
   alert('Nope, but I do like pizza.');
@@ -48,6 +52,7 @@ dogQuestion = dogQuestion.toLowerCase();
 
 if (dogQuestion === 'n' || dogQuestion === 'no') {
   alert('Yes! You remembered that her name is Joy.');
+  count++;
   // console.log('Correct!');
 } else if (dogQuestion === 'y' || dogQuestion === 'yes'){
   alert('Incorrect answer. Her name is Joy.');
@@ -59,33 +64,59 @@ var workBackground = prompt('5. Prior to coming to Code Fellows, did I work as a
 workBackground = workBackground.toLowerCase();
 
 if (workBackground === 'n' || workBackground === 'no') {
+  count++;
   alert('Right. I worked in IT.');
   // console.log('Right!');
 } else if (workBackground === 'y' || workBackground === 'yes') {
-  alert('Wrong. I was never a teacher.');
+  alert('Wrong. I was never a teacher. But, I\'ve thought about it alot.');
 } else {
-  alert('It\'s a yes or no question.')
+  alert('It\'s a yes or no question. Come on. -_-');
 }
 
+//Question 6
 var secretNumber = 9;
-// var userNumber = prompt(firstName + " please guess my secret number(1-10): ");
 
 for (var guess = 0; guess < 4; guess++) {
-  var userNumber = prompt(firstName + " please guess my secret number(1-10): ");
+  var userNumber = prompt('6.' + firstName + ' please guess my secret number(1-10): ');
 
   if (userNumber == secretNumber) {
-    alert("Correct! You did it!");
+    alert('Correct! You did it!');
+    count++;
     break;
   }
   if (userNumber < secretNumber) {
-    alert("Guess a higher number!");
+    alert('Guess a higher number!');
   }
   if (userNumber > secretNumber) {
-    alert("Guess a lower number!");
+    alert('Guess a lower number!');
   }
   if (guess >= 3) {
-    alert("Sorry the correct answer is " + secretNumber + "!");
+    alert('Sorry the correct answer is ' + secretNumber + '!');
   }
 }
+//Question 7
+var fruits = ['grapes', 'plums', 'strawberries', 'oranges', 'pineapples', 'watermelon', 'peaches'];
+var foundMatch = false;
 
-alert('That\'s it! Thank you for getting to know me, ' + firstName + '!!!');
+for (var i = 0; i < 6; i++) { //turns
+  var favoriteFruit = prompt('Guess what kind of fruit I like?');
+  for (var j = 0; j < fruits.length; j++) { //fruit index
+    if (favoriteFruit === fruits[j]) {
+      foundMatch = true;
+      alert('Right! Good job! I do enjoy eating ' + favoriteFruit + '.');
+      count++;
+      break;
+    }
+  }
+  if (foundMatch == true) { //escape loop once a match is found
+    break;
+  }
+  alert("Guess again!"); //wrong answer alert
+  if (i >= 5) {
+    alert('Sorry, you ran out of guesses.' + ' I like to eat ' + fruits[j] + '!');
+
+  }
+
+}
+
+alert('Your score is ' + count + ' out of 7! \n' + 'That\'s it! Thank you for getting to know me, ' + firstName + '!!! Have an awesome day!');
